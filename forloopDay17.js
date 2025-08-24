7d7a3cf01ca27d89bcec771dccf5d4e7b4d3c24c
Raj malviya <rajm45928@gmail.com>
2025-08-24 22:23:23 +0530

    Add files via upload

---
+++ forloopDay17.js
@@ -0,0 +1,13 @@
+console.log("1");
+console.log("2");
+
+for (let i = 1; i<=5; i++) {
+    console.log(i);
+}
+
+for (let i=5; i>=1; i--)  {
+    console.log(i);
+}
+for (let j = 10 ; j>=1; j = j-3 ) {
+    console.log(j);
+}
---
+++ whileloopDay17.js
@@ -0,0 +1,11 @@
+let i = 1;
+while (i<=5){
+    console.log(i);
+    i++;
+}
+
+let j =0;
+while (j<=20) {
+    console.log(j);
+    j++;
+}
